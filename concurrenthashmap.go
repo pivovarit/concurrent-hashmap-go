a90d5a0054f21dd0e9185a246cedf7feeb378990
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-08-07 11:30:48 +0200

    Define default constructor values (#3)

--- concurrenthashmap.go
+++ concurrenthashmap.go
@@ -1,16 +1,24 @@
 package concurrent
 
+const defaultCapacity = 16
+const defaultLoadFactor = 0.75
+const defaultConcurrencyLevel = 1
+
 type HashMap[K comparable, V any] struct {
 }
 
 func New[K comparable, V any]() HashMap[K, V] {
-	return HashMap[K, V]{}
+	return NewWithInitialSize[K, V](defaultCapacity)
 }
 
-func NewWithInitialSize[K comparable, V any](size uint) HashMap[K, V] {
-	return HashMap[K, V]{}
+func NewWithInitialSize[K comparable, V any](capacity uint) HashMap[K, V] {
+	return NewWithLoadFactor[K, V](defaultCapacity, defaultLoadFactor)
 }
 
-func NewWithLoadFactor[K comparable, V any](size uint, load_factor float32) HashMap[K, V] {
+func NewWithLoadFactor[K comparable, V any](capacity uint, load_factor float32) HashMap[K, V] {
+	return NewWithConcurrencyLevel[K, V](defaultCapacity, defaultLoadFactor, defaultConcurrencyLevel)
+}
+
+func NewWithConcurrencyLevel[K comparable, V any](capacity uint, load_factor float32, concurrency_level uint) HashMap[K, V] {
 	return HashMap[K, V]{}
 }
