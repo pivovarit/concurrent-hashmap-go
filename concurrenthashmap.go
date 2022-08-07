ebbd5e18d1adc7819c0d3c74d8c79635de0e5480
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-08-07 16:48:45 +0200

    Define basic object structure (#6)

--- concurrenthashmap.go
+++ concurrenthashmap.go
@@ -5,6 +5,9 @@ const defaultLoadFactor = 0.75
 const defaultConcurrencyLevel = 1
 
 type HashMap[K comparable, V any] struct {
+	initialCapacity  uint
+	loadFactor       float32
+	concurrencyLevel uint
 }
 
 func New[K comparable, V any]() HashMap[K, V] {
@@ -12,13 +15,17 @@ func New[K comparable, V any]() HashMap[K, V] {
 }
 
 func NewWithInitialSize[K comparable, V any](capacity uint) HashMap[K, V] {
-	return NewWithLoadFactor[K, V](defaultCapacity, defaultLoadFactor)
+	return NewWithLoadFactor[K, V](capacity, defaultLoadFactor)
 }
 
-func NewWithLoadFactor[K comparable, V any](capacity uint, load_factor float32) HashMap[K, V] {
-	return NewWithConcurrencyLevel[K, V](defaultCapacity, defaultLoadFactor, defaultConcurrencyLevel)
+func NewWithLoadFactor[K comparable, V any](capacity uint, loadFactor float32) HashMap[K, V] {
+	return NewWithConcurrencyLevel[K, V](capacity, loadFactor, defaultConcurrencyLevel)
 }
 
-func NewWithConcurrencyLevel[K comparable, V any](capacity uint, load_factor float32, concurrency_level uint) HashMap[K, V] {
-	return HashMap[K, V]{}
+func NewWithConcurrencyLevel[K comparable, V any](capacity uint, loadFactor float32, concurrencyLevel uint) HashMap[K, V] {
+	return HashMap[K, V]{
+		concurrencyLevel: concurrencyLevel,
+		initialCapacity:  capacity,
+		loadFactor:       loadFactor,
+	}
 }
